079c46bab09da729740d75580adaa158de851947
Zhang Lu <zhanglu9@gmail.com>
2018-04-27 16:07:41 +0000

    removed learn cookbook and recreated using chef generate cookbook learn

--- cookbooks/learn/.delivery/build_cookbook/recipes/default.rb
+++ cookbooks/learn/.delivery/build_cookbook/recipes/default.rb
@@ -1,5 +1,6 @@
 #
-# Cookbook:: learn
+# Cookbook:: build_cookbook
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+include_recipe 'delivery-truck::default'
--- cookbooks/learn/metadata.rb
+++ cookbooks/learn/metadata.rb
@@ -1,7 +1,20 @@
-name             'learn'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures learn'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+name 'learn'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'All Rights Reserved'
+description 'Installs/Configures learn'
+long_description 'Installs/Configures learn'
+version '0.1.0'
+chef_version '>= 12.14' if respond_to?(:chef_version)
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/learn/issues'
+
+# The `source_url` points to the development repository for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/learn'
